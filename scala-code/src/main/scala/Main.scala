28f366aa190d42f2ec5e55210ed13225e84b427a
Manish Mishra <manish.mishra@knoldus.com>
2023-03-07 17:47:33 +0530

    updated

--- scala-code/src/main/scala/Main.scala
+++ scala-code/src/main/scala/Main.scala
@@ -1,5 +1,9 @@
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
+
+    //Anonymous Function
+    val number: (Int, Int) => String = (firstNum:Int, secondNum:Int)=>firstNum*secondNum+"abc"
+    print(number)
   }
 }
